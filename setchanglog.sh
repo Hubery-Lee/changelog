98f69466a971ce5f78c31b51e5008586b2107429
hubery-lee <hrbeulh@126.com>
2024-11-02 11:04:15 +0800

    fix: deploy setting change

--- setchanglog.sh
+++ setchanglog.sh
@@ -1,5 +1,15 @@
+# install npm
+sudo apt install npm
+npm config set registry http://registry.npmmirror.com
+
 npm init -y
-npm update inflight glob
+
+# install node 23
+curl -fsSL https://deb.nodesource.com/setup_23.x -o nodesource_setup.sh
+sudo -E bash nodesource_setup.sh
+sudo apt-get install -y nodejs
+
+# npm update inflight glob
 # commitlint commit 检查 &&husky git hook 
 npm install --save-dev @commitlint/{cli,config-conventional}
 echo "export default { extends: ['@commitlint/config-conventional'] };" > commitlint.config.js
@@ -8,4 +18,5 @@ npm pkg set scripts.commitlint="commitlint --edit"
 echo "npm run commitlint \${1}" > .husky/commit-msg
 
 # release it 在本地和远程同步changelog
+npm update inflight glob
 npm install release-it @release-it/conventional-changelog --save-dev   
